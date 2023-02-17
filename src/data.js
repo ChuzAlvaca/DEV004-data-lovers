ec4d2f0cb4dde9d454b69c605b4f4b859c434a37
katalimbu <katalina.esteban@gmail.com>
2023-02-17 00:53:07 -0300

    sort desplegado en interfaz

--- src/data.js
+++ src/data.js
@@ -1,6 +1,5 @@
 import ghibli from './data/ghibli/ghibli.js';
-
- export function ordenarMasNueva() {
+export function ordenarMasNueva() {
   const copiaGhibli = [...ghibli.films]
   copiaGhibli.sort(function (a, b) {
     if (parseInt(a.release_date) > parseInt(b.release_date)) {
@@ -13,7 +12,6 @@ import ghibli from './data/ghibli/ghibli.js';
   });
   return copiaGhibli
 }
-console.log(ordenarMasNueva())
 
 export function ordenarMasVieja() {
   const copiaGhibli = [...ghibli.films]
@@ -30,17 +28,9 @@ export function ordenarMasVieja() {
 }
 console.log(ordenarMasVieja())
 
-
- export function ordenarPuntuacionMayor() {
+export function ordenarPuntuacionMayor() {
   const copiaGhibli = [...ghibli.films]
   copiaGhibli.sort((a, b) => { 
-    // parseInt(a.rt_score) - parseInt(b.rt_score)
-    // a.rt_score > b.rt_score ? 1 : a.rt_score < b.rt_score ? -1 : 0
-    /*
-    if (a === b) {
-      return 0;
-    }
-    */
     if (parseInt(a.rt_score) > parseInt(b.rt_score)) {
       return -1;
     }
@@ -51,7 +41,6 @@ console.log(ordenarMasVieja())
   });
   return copiaGhibli
 }
-console.log(ordenarPuntuacionMayor());
 
 export function ordenarPuntuacionMenor() {
  
--- src/main.js
+++ src/main.js
@@ -2,52 +2,64 @@
 import ghibli from './data/ghibli/ghibli.js';
 import {ordenarMasNueva, ordenarMasVieja, ordenarPuntuacionMayor, ordenarPuntuacionMenor} from './data.js';
 
-function showTitleImage() {
+function showTitleImage(movies) {
   const principal = document.getElementById('container');
+  
   if (principal) {
     //este for es para recorrer el array films 
     //parte en 0 , hasta que se recorra todo el arreglo, el i++ es que recorra uno tras otro
-    for (let i = 0; i < ghibli.films.length; i++) {
+    for (let i = 0; i < movies.length; i++) {
       const divColumna = document.createElement('div');
       const creaDiv = document.createElement('div');
       const creaimg = document.createElement('img');
 
       divColumna.setAttribute('class', 'columna');
 
-      creaDiv.innerHTML = ghibli.films[i].title;
+      creaDiv.innerHTML = movies[i].title;
       creaDiv.classList.add("posterTitle");
       // console.log(ghib
       // console.log(ghibli.films)
       // console.log(ghibli.films[i])
       // console.log(ghibli.films[i].title)
-      creaimg.setAttribute('src', ghibli.films[i].poster)
+      creaimg.setAttribute('src', movies[i].poster)
       creaimg.classList.add("posterStyle");
       divColumna.appendChild(creaDiv);
       divColumna.appendChild(creaimg);
       principal.appendChild(divColumna);
+      
     }
   }
 }
-showTitleImage()
+
+showTitleImage(ghibli.films)
 
 const ordenar = document.querySelector('#ordenar');
 
 ordenar.addEventListener('change', (event) => {
-if (event.target.value === "masReciente"){
-  ordenarMasNueva();
-  console.log(ordenarMasNueva())
-}
-if (event.target.value === "menosReciente"){
-  ordenarMasVieja();
-  console.log(ordenarMasVieja())
-}
-if (event.target.value === "mayorRt"){
-  ordenarPuntuacionMayor();
-  console.log(ordenarPuntuacionMayor())
-}
-if (event.target.value === "menorRt"){
-  ordenarPuntuacionMenor();
-  console.log(ordenarPuntuacionMenor())
-}
+  if (event.target.value === "masReciente"){
+    document.getElementById('container').innerHTML = '';
+
+    showTitleImage(ordenarMasNueva())
+    console.log(ordenarMasNueva())
+  }
+  if (event.target.value === "menosReciente"){
+    document.getElementById('container').innerHTML = '';
+
+    showTitleImage(ordenarMasVieja())
+
+    console.log(ordenarMasVieja()) 
+  }
+  if  (event.target.value === "mayorRt"){
+    document.getElementById('container').innerHTML = '';
+
+    showTitleImage(ordenarPuntuacionMayor());
+    console.log(ordenarPuntuacionMayor())
+  } 
+  if (event.target.value === "menorRt"){
+    document.getElementById('container').innerHTML = '';
+
+    showTitleImage(ordenarPuntuacionMenor());
+    console.log(ordenarPuntuacionMenor())
+  }
 
 });
