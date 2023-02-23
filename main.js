571ab51c569fab1691f2f05f22e3aacd2cf0cca2
katalimbu <katalina.esteban@gmail.com>
2023-02-23 14:47:29 -0300

    Updates

--- main.js
+++ main.js
@@ -1,38 +1,82 @@
 
 import ghibli from './data/ghibli/ghibli.js';
+import {filtrarProductor, ordenarMasVieja, ordenarPuntuacionMayor } from './data.js';
+//En el import se borraron las funciones que al final no utilizamos (ordenarMasNueva y ordenarPuntuacionMenor)
 
-function showTitleImage() {
+const showTitleImage = (arrayGhibli) => { // la fn recibe arreglo como parametro
   const principal = document.getElementById('container');
-
+  principal.innerHTML = '';
   if (principal) {
-    //este for es para recorrer el array films 
-    //parte en 0 , hasta que se recorra todo el arreglo, el i++ es que recorra uno tras otro
-    for (let i = 0; i < ghibli.films.length; i++) {
-
+    for (let i = 0; i <  arrayGhibli.length; i++) {
       const divColumna = document.createElement('div');
       const creaDiv = document.createElement('div');
       const creaimg = document.createElement('img');
-
       divColumna.setAttribute('class', 'columna');
-
-      creaDiv.innerHTML = ghibli.films[i].title;
+      creaDiv.innerHTML = arrayGhibli[i].title;
       creaDiv.classList.add("posterTitle");
-
-      // console.log(ghib
-      // console.log(ghibli.films)
-      // console.log(ghibli.films[i])
-      // console.log(ghibli.films[i].title)
-      creaimg.setAttribute('src', ghibli.films[i].poster)
+      creaimg.setAttribute('src', arrayGhibli[i].poster)
       creaimg.classList.add("posterStyle");
       divColumna.appendChild(creaDiv);
       divColumna.appendChild(creaimg);
       principal.appendChild(divColumna);
     }
-
-
   }
 }
-showTitleImage()
+showTitleImage(ghibli.films); // enviamos arreglo como argumento
 
-
-// console.log(ghibli);
+const ordenar = document.querySelector('#ordenar');
+ordenar.addEventListener('change', (event) => {
+  if (event.target.value === "menosReciente"){
+    document.getElementById("container").innerHTML = "";
+    const filmsOrdenadas = ordenarMasVieja(ghibli.films) // [5,4,3].reverser() === [3,4,5]
+    showTitleImage(filmsOrdenadas);
+    //console.log(filmsOrdenadas)
+  }
+  if (event.target.value === "masReciente"){
+    const filmsOrdenadasReverse = ordenarMasVieja(ghibli.films).reverse()
+    showTitleImage(filmsOrdenadasReverse )
+    // console.log(ordenarMasNueva(ghibli.films),filmsOrdenadasReverse )
+  }
+  if (event.target.value === "mayorRt"){
+    document.getElementById("container").innerHTML = "";
+    showTitleImage(ordenarPuntuacionMayor(ghibli.films))
+    // console.log(ordenarPuntuacionMayor(ghibli.films))
+  }
+  if (event.target.value === "menorRt"){
+    document.getElementById("container").innerHTML = "";
+    const filmsMejorPuntuadasReverse = ordenarPuntuacionMayor(ghibli.films).reverse()
+    showTitleImage(filmsMejorPuntuadasReverse) // envias argumento 
+  }
+});
+const filtrar = document.querySelector('#filtrar');
+filtrar.addEventListener('change', (event) => {
+  if (event.target.value === 'Isao Takahata') {
+    document.getElementById("container").innerHTML = "";
+    const filmsFiltradas = filtrarProductor(ghibli.films, 'Isao Takahata') 
+    showTitleImage(filmsFiltradas);
+  }
+  if (event.target.value === 'Toshio Suzuki') {
+    document.getElementById("container").innerHTML = "";
+    const filmsFiltradas = filtrarProductor(ghibli.films, 'Toshio Suzuki') 
+    showTitleImage(filmsFiltradas);
+  }
+  if (event.target.value === 'Hayao Miyazaki') {
+    document.getElementById("container").innerHTML = "";
+    const filmsFiltradas = filtrarProductor(ghibli.films, 'Hayao Miyazaki') 
+    showTitleImage(filmsFiltradas);
+  }
+  if (event.target.value === 'Toru Hara') {
+    document.getElementById("container").innerHTML = "";
+    const filmsFiltradas = filtrarProductor(ghibli.films, 'Toru Hara') 
+    showTitleImage(filmsFiltradas);
+  }
+  if (event.target.value === 'Yoshiaki Nishimura') {
+    document.getElementById("container").innerHTML = "";
+    const filmsFiltradas = filtrarProductor(ghibli.films, 'Yoshiaki Nishimura') 
+    showTitleImage(filmsFiltradas);
+  }
+  if (event.target.value === 'todas') {
+    document.getElementById("container").innerHTML = "";
+    showTitleImage(ghibli.films);
+  }
+});
