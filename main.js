ed702703b5620f0a777cd5b118e30811d4131192
katalimbu <katalina.esteban@gmail.com>
2023-02-27 10:55:55 -0300

    Updates

--- main.js
+++ main.js
@@ -80,3 +80,131 @@ filtrar.addEventListener('change', (event) => {
     showTitleImage(ghibli.films);
   }
 });
+
+// definir funcion 
+//input --codigo ---output 
+//hacer que el código se vuelva un objeto 
+// function printImages (arreglo){
+//   console.log(arreglo)
+// }
+//for tradicional recorre cada pelicula, nos permite acceder a un primer nivel de propiedades .film
+function showPersonajes (title) {
+  const arrOriginal = ghibli.films;
+  for (let i = 0; i < arrOriginal.length; i++){
+    const film = arrOriginal[i];{
+      // console.log(film.people) 
+      if (film.title === title){
+        showPersonajes2(film.people);
+      }
+    }
+  }
+}
+
+
+
+// showSelect(ghibli.films) 
+const selector = (arrayGhibli) =>{
+  const values = (arrayGhibli)
+  const $select = document.getElementById("personajes")
+  //empty option
+  const $option = document.createElement("option")
+  $option.value = ""
+  $option.innerHTML = "Por Pelicula"
+  $select.appendChild($option)
+  values.forEach(values => {
+    const $option = document.createElement("option")
+    $option.value = values.title
+    $option.innerHTML = values.title
+    $select.appendChild($option)
+  })
+}
+selector(ghibli.films)
+
+const $select = document.getElementById("personajes")
+$select.addEventListener('change',function(){
+  // console.log($select.value)
+  document.getElementById("container").innerHTML = "";
+  showPersonajes($select.value)
+});
+
+
+// / funcion peronajes
+const showPersonajes2 = (personajes) => { // la fn recibe arreglo como parametro
+  const principal = document.getElementById('container');
+  // const numeroDePersonajes = document.getElementById('numero-de-personajes');
+  console.log(personajes)
+  if (principal) {
+    for (let i = 0; i < personajes.length; i++) {
+      const divColumna = document.createElement('div');
+      const creaDiv = document.createElement('div');
+      const creaimg = document.createElement('img');
+      divColumna.setAttribute('class', 'columna');
+      creaDiv.innerHTML =personajes[i].name;
+      creaDiv.classList.add("posterTitle");
+      creaimg.setAttribute('src',personajes[i].img)
+      creaimg.classList.add("posterStyle");
+      divColumna.appendChild(creaDiv);
+      divColumna.appendChild(creaimg);
+      principal.appendChild(divColumna);
+    }
+  }
+}
+
+
+
+
+
+//const arrayPrueba = [];
+
+/*
+  film.people.forEach(function (personaje,) {
+    // console.log ([film.id,film.title, personaje.name, personaje.img]);
+    const nuevoObjeto = {film.id,film.title, personaje.name, personaje.img};
+    console.log (nuevoObjeto); 
+    arrayPrueba.push(nuevoObjeto)
+  });*/
+
+
+
+
+// let greaterTen2 = numbers.filter(number => number > 10 );
+
+
+
+//     
+//   
+// 
+// método de iteración map
+
+
+
+
+//forma de acceder, muy rudimentariamente, los datos son string puros.
+// console.log(ghibli.films[0].people)
+
+//asi llegas a los datos puros en forma string: 
+// ghibli.films.forEach(function (film) {
+//   film.people.forEach(function (personaje,) {
+//     console.log ([film.title, personaje.name, personaje.img]);
+//   });
+// });
+
+// const showSelect = (arrayGhibli) => { // la fn recibe arreglo como parametro
+//   const select = document.getElementById('personajes');
+//   if (select) {
+//     for (let i = 0; i <  arrayGhibli.length; i++) {
+//       const creaDiv = document.createElement('div');
+//       creaDiv.innerHTML = arrayGhibli[i].title;
+//       creaDiv.classList.add("movieTitle");
+      
+      
+//     }
+//   }
+// 
+// showSelect(ghibli.films)
+
+
+
+
+
+
